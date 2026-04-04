9e6562dbb91a5cdc30ff4fc466b04c29dc98c47c
ternemi13 <wilsperez13@gmail.com>
2026-04-03 19:37:03 -0500

    lee config.txt y lo muestra en consola

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
 
-int opcion;
+    int opcion;
+
     do {
         cout << "\n=== SISTEMA DE LIGA ===\n";
         cout << "1. Ver tabla de posiciones\n";
@@ -15,27 +18,47 @@ int opcion;
         cin >> opcion;
 
         switch(opcion) {
-            case 1:
-                cout << "Mostrando tabla...\n";
+            case 1: {
+                ifstream archivo("data/config.txt");
+
+                if (!archivo) {
+                    cout << "Error al abrir config.txt\n";
+                    break;
+                }
+
+                string linea;
+
+                cout << "\nContenido de config.txt:\n";
+
+                while (getline(archivo, linea)) {
+                    cout << linea << endl;
+                }
+
+                archivo.close();
                 break;
+            }
+
             case 2:
                 cout << "Registrando partido...\n";
                 break;
+
             case 3:
                 cout << "Mostrando jornadas...\n";
                 break;
+
             case 4:
                 cout << "Mostrando partidos...\n";
                 break;
+
             case 5:
                 cout << "Saliendo del programa...\n";
                 break;
+
             default:
                 cout << "Opcion invalida\n";
         }
 
     } while(opcion != 5);
-
 
     return 0;
 }
