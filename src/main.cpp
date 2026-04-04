219717a9cf7b37de88eeb04a2c8d6a60be8857ba
ternemi13 <wilsperez13@gmail.com>
2026-04-03 19:16:37 -0500

    Primer menu

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,40 @@
 using namespace std;
 
 int main() {
-    cout << "Sistema de Liga iniciado" << endl;
+
+int opcion;
+    do {
+        cout << "\n=== SISTEMA DE LIGA ===\n";
+        cout << "1. Ver tabla de posiciones\n";
+        cout << "2. Registrar partido\n";
+        cout << "3. Ver jornadas\n";
+        cout << "4. Ver partidos\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch(opcion) {
+            case 1:
+                cout << "Mostrando tabla...\n";
+                break;
+            case 2:
+                cout << "Registrando partido...\n";
+                break;
+            case 3:
+                cout << "Mostrando jornadas...\n";
+                break;
+            case 4:
+                cout << "Mostrando partidos...\n";
+                break;
+            case 5:
+                cout << "Saliendo del programa...\n";
+                break;
+            default:
+                cout << "Opcion invalida\n";
+        }
+
+    } while(opcion != 5);
+
+
     return 0;
 }
